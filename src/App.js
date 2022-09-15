af3f4400c67c9c3c231b1320a14a63e1e28beb00
JelicaMitic <mitic.jelica22@gmail.com>
2022-09-15 18:41:34 +0200

    Kreirana ruta za pocetnu stranu

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 import BioskopPocetna from './strane/BioskopPocetna';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
 
 function App() {
   return (
     <div className="App">
-      <BioskopPocetna />
+      <Router>
+        <Routes>
+          <Route path="/" element={<BioskopPocetna />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
